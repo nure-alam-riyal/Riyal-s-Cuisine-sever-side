5821cb9efd25684abfd6aa06f77662e89c21e777
nurealamriyal <nurealamriyal@gmail.com>
2024-12-22 03:03:23 +0600

    find one and find all data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port=process.env.PORT || 1507
 const app=express()
 // fYwV0dAYU9VHOZaX
@@ -9,7 +10,7 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nurealamriyal.adrs4.mongodb.net/?retryWrites=true&w=majority&appName=nurealamriyal`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +24,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const foodsCollection=client.db('resturant').collection('foods')
+    app.get('/foods',async (req,res) => {
+      const result=await foodsCollection.find().toArray()
+      res.send(result)
+    })
+     app.get('/food/:id',async (req,res) => {
+      const id=req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await foodsCollection.findOne(query)
+      res.send(result)
+     })
+    app.post('/add-food',async (req,res) => {
+        const foods=req.body
+        const result=await foodsCollection.insertOne(foods)
+        res.send(result)
+
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -30,7 +48,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -38,6 +56,7 @@ run().catch(console.dir);
 app.get('/',(req,res)=>{
     res.send('managements sysytem here')
 })
+
 app.listen(port,()=>{
     console.log(`server run on the port ${port}`)
 })
